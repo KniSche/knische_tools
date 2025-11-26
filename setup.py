2515450f62178cc213d7a58001d7767007e48f6c
Vincent Knight-Schrijver <vincent.knightschrijver@gmail.com>
2025-11-26 13:35:42 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='knische_tools',              # Name of the package
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),          # Automatically finds the 'knische_tools' folder
     install_requires=[                 # Dependencies
         'scanpy',
